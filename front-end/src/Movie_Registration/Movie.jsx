9e3db5fcc22d4797df0c7408492255dbc2e690c4
Jason Daniel <jasondaniel18@gmail.com>
2023-07-05 12:25:30 +0530

    fixed deployment

--- front-end/src/Movie_Registration/Movie.jsx
+++ front-end/src/Movie_Registration/Movie.jsx
@@ -23,7 +23,7 @@ export default function MovieForm() {
   };
 
   const registerMovie = async () => {
-    const result = await fetch("http://server-five-wine.vercel.app//api/movie/register", {
+    const result = await fetch("https://server-five-wine.vercel.app/api/movie/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
